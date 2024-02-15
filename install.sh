e0605d6a6822775bd18a62ee3e0ac7dc2bd011ac
heabeounMKTO <heabeoun69zin@gmail.com>
2024-02-15 17:39:46 +0700

    install lunarvim + tmux

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/bash
 
 apt-get install ninja-build gettext cmake unzip curl git
+git clone https://github.com/neovim/neovim
+cd neovim 
+git checkout main
+make CMAKE_BUILD_TYPE=Release
+make install
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+
 apt install tmux
-cp . ~/
+cp .tmux.conf ~/
+cp .zshrc
+
+

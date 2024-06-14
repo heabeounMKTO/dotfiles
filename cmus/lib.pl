4189b1105270d599b835c4f7271ca3205b40723b
heabeounMKTO <heabeoun69zin@gmail.com>
2024-06-14 14:03:48 +0700

    fuq

---
+++ cmus/lib.pl
@@ -0,0 +1,63 @@
+/media/hbpopos/penisf/soulseek_downloads/complete/deviusdavius/Plastic Beach [24]/10 On Melancholy Hill.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bubthenub/A Legendary Christmas/06 Christmas in New Orleans.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/01 - Kanye West - Every Hour.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/03 - Kanye West - Follow God.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/06 - Kanye West - Everything We Need.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/07 - Kanye West - Water.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/08 - Kanye West - God Is.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/09 - Kanye West - Hands On.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/JESUS IS KING (2019)/11 - Kanye West - Jesus Is Lord.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_01_Cool Yule.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_02_'Zat You, Santa Claus! (single version).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_03_Christmas Night In Harlem (single version).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_04_White Christmas (single version).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_05_Christmas in New Orleans.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_06_Baby It's Cold Outside.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_07_I've Got My Love to Keep Me Warm.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_08_Winter Wonderland (single version).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/davo1624/Cool Yule (deluxe edition)/Louis Armstrong_Cool Yule (deluxe edition)_09_What a Wonderful World.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/pactdisco/flacurrent 2415/Sabrina Carpenter - Espresso.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/17 - EVERYTHING MUST GO.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/18 - STUNTMAN (feat. Vince Staples).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/19 - WHAT A DAY.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/20 - WHARF TALK (feat. A$AP Rocky).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/21 - DOGTOOTH.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/22 - HEAVEN TO ME.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/23 - BOYFRIEND, GIRLFRIEND (2020 Demo) (feat. YG).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/AtomicWRLD/CALL ME IF YOU GET LOST The Estate Sale/24 - SORRY NOT SORRY.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 01 - IGOR’S THEME.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 02 - EARFQUAKE.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 03 - I THINK.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 04 - EXACTLY WHAT YOU RUN FROM YOU END UP CHASING.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 05 - RUNNING OUT OF TIME.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 06 - NEW MAGIC WAND.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 07 - A BOY IS A GUN.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 08 - PUPPET.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 09 - WHAT’S GOOD.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 10 - GONE, GONE + THANK YOU.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 11 - I DON’T LOVE YOU ANYMORE.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/roorooroo/Tyler, the Creator - IGOR/Tyler, the Creator - IGOR - 12 - ARE WE STILL FRIENDS.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/01 - Tyler, The Creator - SIR BAUDELAIRE (feat. DJ Drama).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/02 - Tyler, The Creator - CORSO (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/03 - Tyler, The Creator - LEMONHEAD (feat. 42 Dugg).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/04 - Tyler, The Creator - WUSYANAME (feat. Youngboy Never Broke Again & Ty Dolla $ign).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/05 - Tyler, The Creator - LUMBERJACK.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/06 - Tyler, The Creator - HOT WIND BLOWS (feat. Lil Wayne) (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/07 - Tyler, The Creator - MASSA.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/08 - Tyler, The Creator - RUNITUP (feat. Teezo Touchdown).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/09 - Tyler, The Creator - MANIFESTO (feat. Domo Genesis) (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/10 - Tyler, The Creator - SWEET - I THOUGHT YOU WANTED TO DANCE (feat. Brent Faiyaz & Fana Hues) (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/11 - Tyler, The Creator - MOMMA TALK.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/12 - Tyler, The Creator - RISE! (feat. DAISY WORLD).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/13 - Tyler, The Creator - BLESSED (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/14 - Tyler, The Creator - JUGGERNAUT (feat. Lil Uzi Vert & Pharrell Williams) (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/15 - Tyler, The Creator - WILSHIRE.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/16 - Tyler, The Creator - SAFARI (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/17 - Tyler, The Creator - EVERYTHING MUST GO.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/18 - Tyler, The Creator - STUNTMAN (feat. Vince Staples).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/19 - Tyler, The Creator - WHAT A DAY.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/20 - Tyler, The Creator - WHARF TALK (feat. A$AP Rocky) (Explicit).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/21 - Tyler, The Creator - DOGTOOTH.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/22 - Tyler, The Creator - HEAVEN TO ME.flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/23 - Tyler, The Creator - BOYFRIEND, GIRLFRIEND (feat. YG) (2020 Demo).flac
+/media/hbpopos/penisf/soulseek_downloads/complete/bandit<3/CALL ME IF YOU GET LOST- The Estate Sale (2021)/24 - Tyler, The Creator - SORRY NOT SORRY.flac
